a486a3eca8023b4a1248314915840ccc08a6899d
dj <douglas.macdonald.jr@gmail.com>
2017-02-11 00:35:24 -0700

    added cache directory

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -6,5 +6,6 @@ CarrierWave.configure do |config|
   }
 
   config.fog_directory  = ENV['AWS_BUCKET']
+  config.cache_dir     = "#{Rails.root}/tmp/uploads"   # For Heroku
   config.fog_public     = false
 end
